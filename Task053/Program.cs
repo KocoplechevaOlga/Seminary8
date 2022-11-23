73819191b2d31bb59c2248fd5b991696b06e02cb
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-23 21:55:09 +0300

    Task 53

--- Task053/Program.cs
+++ Task053/Program.cs
@@ -1,4 +1,57 @@
 ﻿// Задайте двумерный массив. Напишите программу, 
 //которая поменяет местами первую и последнюю строку массива.
 
-Console.WriteLine("Hello, World!");
+int[,] FillMatrixRnd(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd  = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = rnd.Next(min, max);
+            }
+    } 
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 5} |");
+            else Console.Write($"{matrix[i,j], 5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] RierstLastRemote(int [,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int c = matrix[0,j];
+        matrix[0,j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = c;
+    }
+return matrix;
+}
+
+Console.WriteLine("Введите количество строк:");
+int matrixRows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int matrixColums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное ограничение массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное ограничение массива:");
+int b = Convert.ToInt32(Console.ReadLine())+1;
+int [,] myMatrix = FillMatrixRnd(matrixRows, matrixColums, a, b);
+PrintMatrix(myMatrix);
+Console.WriteLine(" ");
+
+int [,] newMatrix = RierstLastRemote(myMatrix);
+PrintMatrix(newMatrix);
+Console.WriteLine(" ");
